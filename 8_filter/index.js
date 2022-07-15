dc37dbbfa5769e0314748acb0fba72c9209a21a0
Aleff Junio Silva Lins Lira <dev.alefflira@gmail.com>
2022-07-15 20:14:28 -0300

    Adicionado os arquivos relativos a Array.filter

---
+++ 8_filter/index.js
@@ -0,0 +1,16 @@
+import { getPeople } from './starWarsService.js';
+
+async function main() {
+  const starWarsPeople = await getPeople(1);
+  const starWarsPeopleFiltered = starWarsPeople
+    .filter(person => {
+      return person.gender === 'male';
+    })
+    .map(person => {
+      return { name: person.name, gender: person.gender };
+    });
+
+  console.log(starWarsPeopleFiltered);
+}
+
+main();
